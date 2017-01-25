a3c819e13ea47ecdadb9f644850b73ab19873124
sepefeets <rosbot@outlook.com>
2017-01-25 04:42:24 -0800

    [1460] fix heraldvolazj crash

--- cards/Sim_OG_316.cs
+++ cards/Sim_OG_316.cs
@@ -11,20 +11,21 @@ namespace HREngine.Bots
 		public override void getBattlecryEffect(Playfield p, Minion own, Minion target, int choice)
         {
             List<Minion> temp = (own.own) ? p.ownMinions : p.enemyMinions;
-            int pos = temp.Count;
-            if (pos > 1)
-            {
-                foreach (Minion m in (own.own) ? p.ownMinions.ToArray() : p.enemyMinions.ToArray())
-                {
-                    if (m.entityID == own.entityID) continue;
-                    pos = (own.own) ? p.ownMinions.Count : p.enemyMinions.Count;
-                    if (pos > 6) break;
-                    p.callKid(m.handcard.card, pos, own.own, true);
-                    temp = (own.own) ? p.ownMinions : p.enemyMinions;
-                    temp[pos].Hp = 1;
-                    temp[pos].Angr = 1;
-                }
-            }
+		    foreach (Minion m in (own.own) ? p.ownMinions.ToArray() : p.enemyMinions.ToArray())
+		    {
+		        if (m.entityID == own.entityID) continue;
+		        int pos = (own.own) ? p.ownMinions.Count : p.enemyMinions.Count;
+		        p.callKid(m.handcard.card, pos, own.own, true);
+		        if (pos < 6)
+		        {
+		            Minion kidminion = temp[own.zonepos - 1]; //volazj isn't on the playfield yet but the kid is where volazj will be
+
+		            int angr = 1 - kidminion.Angr;
+		            int hp = 1 - kidminion.maxHp;
+
+		            p.minionGetBuffed(kidminion, angr, hp);
+		        }
+		    }
         }
     }
 }
